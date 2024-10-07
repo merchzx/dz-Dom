ac153ae30bbf8877418109fcf7a86108ebd7be3e
ArtemRudomanenko <artemrudomanenko@gmail.com>
2024-10-07 16:42:45 +0300

    FULL GOTOVO

---
+++ script.js
@@ -0,0 +1,11 @@
+let highDiv = document.getElementById("highDiv");
+let midleDiv = document.getElementById("midleDiv");
+let lowDiv = document.getElementById("lowDiv");
+
+let globalDiv = document.getElementsByClassName("globalDiv");
+
+let button = document.getElementsByTagName("button")
+
+highDiv.setAttribute("style","background:yellow; width:900px;border-bottom-width:1px;border-bottom-style:solid;border-bottom-color:black;")
+midleDiv.setAttribute("style","background:yellow; width:900px;border-bottom-width:1px;border-bottom-style:solid;border-bottom-color:black;position:relative;top:-20px")
+lowDiv.setAttribute("style","background:yellow; width:900px;position:relative;top:-40px")
